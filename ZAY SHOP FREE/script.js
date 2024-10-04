6631a52e88303e0cfa0be7207acfc7c13005eb5f
Jahnavi chauhan <chauhankhushi.16775@gmail.com>
2024-10-04 10:15:32 +0530

    last week new changes

--- ZAY SHOP FREE/script.js
+++ ZAY SHOP FREE/script.js
@@ -1,3 +1,18 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+let menuOpen = flase;
+
+hamburger.addEventListener("click", () => {
+  if (menuOpen == flase) {
+    navLinks.style.display = "block";
+    menuOpen = true;
+  } else if (menuOpen == true) {
+    navLinks.style.display = "none";
+    menuOpen = false;
+  }
+});
+
 document.addEventListener("DOMContentLoaded", () => {
   // Example of adding functionality for search or cart
   const searchLink = document.querySelector('a[href="#search"]');
